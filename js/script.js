91a1c1ac38aacff19b00baf4321e73f3814e3a59
Sarah-El <sarah-louise.ellis@hotmail.co.uk>
2022-12-16 20:03:15 +0000

    added code to change row color based on time

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Add current date to jumbotron 
 
 var currentDate = moment().format("dddd[,] Do MMM"); 
-console.log(currentDate);
 $("#currentDay").text(currentDate);
+
+// Change the colour of rows based on times
+
+let currentTime = moment().format("H");
+currentTime = parseInt(currentTime); // Converting string to a number
+
+var times = $("#nine")
+let rowTimeNine = times.attr("data-time");
+rowTimeNine = parseInt(rowTimeNine);
+if (rowTimeNine > 12) {
+    rowTimeNine = rowTimeNine + 12
+}
+var textbox = $("#text-box-9am");
+
+if (parseInt(rowTimeNine) < currentTime) {
+    textbox.css({"background-color" : "red"})
+}
+
