3889531ed154f819865001c1634055aa3f9bf579
Sarah-El <sarah-louise.ellis@hotmail.co.uk>
2023-01-31 17:27:40 +0000

    updated README file using README generator

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# Work-Day-Scheduler
+# Workday Scheduler
+
+  ![Github licence](http://img.shields.io/badge/license-None-blue.svg)
+
+  ## Description
+  this project is a daily workday scheduler designed to keep track of meetings during the standard working hours.
+
+  ## Table of Contents
+  * [Description] (#description)
+  * [Installation] (#installation)
+  * [Usage] (#usage)
+  * [Licence] (#license)
+  * [Contributing] (#contributing)
+  * [Tests] (#testing)
+  * [Questions] (#questions)
+
+  ## Description
+  this project is a daily workday scheduler designed to keep track of meetings during the standard working hours.
+
+  ## Installation
+  N/A
+
+  ## Usage
+  The application should be used to schedule timings and appointmemts during working hours and days. You can click on each hours text area to input information and click the 'save' button to save this to your schedule. 
+
+  ## License
+  This project uses the following license None
+
+  ## Contributing
+  N/A
+
+  ## Tests
+  N/A
+
+  ## Questions
+  For questions about this application please reach out to me using one of the following methods:
+
+  GitHub: Sarah-El
+  Email: sarahlosbourne@gmail.com
--- js/script.js
+++ js/script.js
@@ -3,20 +3,70 @@
 var currentDate = moment().format("dddd[,] Do MMM"); 
 $("#currentDay").text(currentDate);
 
+// Save user input to local storage
+$(".saveBtn").on('click', function (event) {
+    
+})
+
 // Change the colour of rows based on times
 
-let currentTime = moment().format("H");
-currentTime = parseInt(currentTime); // Converting string to a number
+var textBox = $("textarea");
+// var scheduleTime = $(this).attr("id").split("-")[1];
+var currentHour = moment().format('hA');
+console.log(currentHour);
 
-var times = $("#nine")
-let rowTimeNine = times.attr("data-time");
-rowTimeNine = parseInt(rowTimeNine);
-if (rowTimeNine > 12) {
-    rowTimeNine = rowTimeNine + 12
-}
-var textbox = $("#text-box-9am");
+textBox.each(function() {
+    if ($(this).attr('textBox'))
+})
 
-if (parseInt(rowTimeNine) < currentTime) {
-    textbox.css({"background-color" : "red"})
-}
+function colorRow () {
+    if (scheduleTime > currentHour) {
+        textbox.css({"background-color" : "grey"})
+    } else if (scheduleTime < currentHour) {
+        textbox.css({"background-color" : "green"})
+    } else {textbox.css({"background-color" : "red"})}
+};
+
+
+
+
+// let currentTime = moment().format("h A");
+// currentTime = parseInt(currentTime); // Converting string to a number
+
+// console.log(currentTime);
+
+// var timeNine = $("#nine")
+// let rowTimeNine = timeNine.attr("data-time");
+// rowTimeNine = parseInt(rowTimeNine);
+// if (rowTimeNine > 12) {
+//     rowTimeNine = rowTimeNine + 12
+// };
+// var textbox = $("#text-box-9am");
+
+// if (rowTimeNine < currentTime) {
+//     textbox.css({"background-color" : "gray"})
+//     console.loh("hello");
+// } else if (rowTimeNine > currentTime) {
+//     textbox.css({"background-color" : "green"})
+// } else {
+//     textbox.css({"background-color" : "red"})
+// };
+
+// var timeTen = $("#ten")
+// let rowTimeTen = timeTen.attr("data-time");
+// rowTimeTen = parseInt(rowTimeTen);
+// if (rowTimeTen > 12) {
+//     rowTimeTen = rowTimeTen + 12
+// };
+// var textbox = $("#text-box-10am");
+
+// if (rowTimeTen < currentTime) {
+//     textbox.css({"background-color" : "grey"})
+// } else if (rowTimeTen > currentTime) {
+//     textbox.css({"background-color" : "green"})
+// } else if (rowTimeTen === currentTime) {
+//     textbox.css({"background-color" : "red"})
+// } else {
+//     textbox.css({"background-color" : "white"})
+// };
 
