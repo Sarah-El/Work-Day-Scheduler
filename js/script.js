819bb89edd33f482b40f9fba29c06e37f6b912dc
Sarah-El <sarah-louise.ellis@hotmail.co.uk>
2023-02-09 21:18:32 +0000

    created function to save user input to local storage

--- js/script.js
+++ js/script.js
@@ -5,5 +5,7 @@ $("#currentDay").text(currentDate);
 
 // Save user input to local storage
 $(".saveBtn").on('click', function (event) {
-    
-})
+    var time = $(this).parent().attr('id');
+    var data = $(this).siblings('description').val();
+    localStorage.setItem(time, data);
+});
