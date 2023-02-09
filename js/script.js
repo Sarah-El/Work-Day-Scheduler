c78b1f847e7fd4f13826ba867a9b0683999af37a
Sarah-El <sarah-louise.ellis@hotmail.co.uk>
2023-02-09 21:28:30 +0000

    added function and if else statements to update each time block based on current time

--- js/script.js
+++ js/script.js
@@ -9,3 +9,19 @@ $(".saveBtn").on('click', function (event) {
     var data = $(this).siblings('description').val();
     localStorage.setItem(time, data);
 });
+
+// Change colour of timeblocks based on current time using css
+
+var currentTime = moment().hours();
+$('.row').each(function () {
+    var timeBlock = parseInt($(this).attr('id').split('-')[1]);
+    if (timeBlock === currentTime) {
+        $(this).addClass('present');
+    } else if (timeBlock < currentTime) {
+        $(this).removeClass('present');
+        $(this).addClass('past');
+    } else {
+        $(this).removeClass('past');
+        $(this).addClass('future');
+    }
+})
